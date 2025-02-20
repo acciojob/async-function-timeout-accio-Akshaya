3ab0af439356bbcc03b5f7efe5b103301e8d1852
accio-Akshaya <akshayaganesh0910@gmail.com>
2025-02-20 09:00:44 -0800

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-document.getElementById("btn").addEventListener("click",async function(){
-	const text = document.getElementById("text").value;
-	const delay = parseInt(document.getElementById("delay").value,10);
-	const output = document.getElementById("output");
+document.getElementById("btn").addEventListener("click", async function () {
+    const text = document.getElementById("text").value;
+    const delay = parseInt(document.getElementById("delay").value, 10);
+    const output = document.getElementById("output");
 
-	output.textContent ="";
+    output.textContent = "";
 
-	if(!text || isNaN(delay) || delay < 0){
-		output.textContent = "Please enter valid text and a positive delay.";
-		return;
-		
-	}
+    if (!text || isNaN(delay) || delay < 0) {
+        output.textContent = "Please enter valid text and a positive delay.";
+        return;
+    }
 
-	function delayMessage(ms){
-		return new Promise(resolve =>setTimeout(resolve,ms));
-		
-	}
-	await delayMessage(delay);
-	output.textContent ="text";
+    function delayMessage(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    await delayMessage(delay);
+    output.textContent = text;
 });
