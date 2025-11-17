ddae61679ce154b723486833604922c61dc747c4
Akshaya <akshayaganesh0910@gmail.com>
2025-11-17 10:17:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+const btn = document.getElementById("btn");
+const output = document.getElementById("output");
+
+function wait(ms){
+	return new Promise(resolve=>setTimeout(resolve,ms));
+}
+
+bt.addEventListener("click",async()=>{
+	const text = document.getElementById("text").value;
+	const delay = Number(document.getElementById("delay").value);
+
+	output.textContent = "";
+	await wait(delay);
+
+	output.textContent = text;
+});
